60f40afabade1bf0d73cb717d297906f04205e78
spardit <jamemay19@gmail.com>
2017-11-19 14:49:29 +0700

    Testing upload to github

---
+++ src/oop_lap/s357411110060/rmutsv/com/HelloWorld.java
@@ -0,0 +1,14 @@
+package oop_lap.s357411110060.rmutsv.com;
+
+public class HelloWorld {
+
+//    create main_method
+public static void main(String[] args) {
+
+    System.out.println("Hello World,I'am Nook");
+
+
+
+}//main
+
+}// class
