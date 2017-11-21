044f527d8da5ffcab44686a2efb082ab231c1e15
Piyanut23 <Piyanut642@hotmail.com>
2017-11-21 11:19:48 +0700

    Test Variable

--- src/oop_lap/s357411110060/rmutsv/com/HelloWorld.java
+++ src/oop_lap/s357411110060/rmutsv/com/HelloWorld.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 
     System.out.println("Hello World,I'am Nook");
     System.out.println("Piyanut Raksin");
-    System.out.println("information system,MT:RMUTSV");
+    System.out.println("information system,MT:RMUTSV☺☺");
 
 
 
