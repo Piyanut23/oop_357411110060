0f1655fa844ecd69a3c9a3c2858653aa9ada2a40
Piyanut23 <Piyanut642@hotmail.com>
2017-11-28 09:33:48 +0700

    Test condition

--- src/oop_lab2/TestInput.java
+++ src/oop_lab2/TestInput.java
@@ -1,11 +1,12 @@
 package oop_lab2;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class TestInput {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // BufferedReader
         BufferedReader reader = new BufferedReader (
