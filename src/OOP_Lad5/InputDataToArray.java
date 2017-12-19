1ee008af4ff5a1e5f7fe493d1556f7f324bde72c
Piyanut23 <Piyanut642@hotmail.com>
2017-12-19 12:06:28 +0700

    Test InputDataArray

--- src/OOP_Lad5/InputDataToArray.java
+++ src/OOP_Lad5/InputDataToArray.java
@@ -8,39 +8,39 @@ package OOP_Lad5;
 import java.util.Scanner;
 
 public class InputDataToArray {
-    private static final int MaX = 10;
+    private static final int MAX = 10;
 
     public static void main(String[] args) {
-        int num []=new int[MaX];
-        inputData (num);
+        int num[] = new int[MAX];
         num = inputData(num);
-        showData (num);
-        summation(num);
+        showData(num);
+
+        summation (num);
+
 
     }//main
 
     private static void summation(int[] num) {
-        int total =0;
-        for (int i=0;i<num.length;i++ ){
-            total +=num[i];
+        int total = 0;
+        for (int i=0;i<num.length;i++){
+            total += num[i];
         }
-        System.out.println("The summation of"+ "Value in array is :+total");
-        System.out.println("The average Value " + "in array is:"+total/MaX);
+        System.out.println("\n10The summation of" + "value in array is: "+total);
+        System.out.println("The average value" + "in array is: "+total/MAX);
     }
-
 
     private static void showData(int[] num) {
-        System.out.println("Data in array:   ");
-        for (int val : num)
-            System.out.print(val+"  ");
+        System.out.println("Data in array: ");
+        for (int val: num)
+            System.out.print(val+" ");
     }
 
-    private static int[] inputData(int []num) {
+    private static int[] inputData(int[] num) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Plese enter data to array: ");
         for (int i=0;i<num.length;i++){
-            System.out.print("num["+i+"]:");
-            num[i]= scanner.nextInt();
+            System.out.print("number["+i+"]: ");
+            num[i] = scanner.nextInt();
         }
         return num;
     }
